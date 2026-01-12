31bcc249d76c683df882ab2a300adbc22a00969d
hanifnurfai <106461708+hanifnurfai@users.noreply.github.com>
2026-01-12 07:39:14 +0700

    local download refactoring

--- tool/download.py
+++ tool/download.py
@@ -1,2 +1,11 @@
-import request
-import 
+import requests
+from bs4 import BeautifulSoup
+
+
+
+MCBE_url = "https://www.minecraft.net/en-us/download/server/bedrock"
+MCJAVA_url = "https://www.minecraft.net/en-us/download/server/"
+
+content = requests.get(MCBE_url)
+#data = BeautifulSoup(content, "lxml")
+print(content.status_code)
