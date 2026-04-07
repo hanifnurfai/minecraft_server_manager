ae84580c10ed86bcc1c2444d53a7159861435f17
hanifnurfai <106461708+hanifnurfai@users.noreply.github.com>
2026-04-07 14:48:47 +0700

    refactore tool/download.py

--- tool/download.py
+++ tool/download.py
@@ -1,42 +1,64 @@
+import os
+import time
 import requests
-from bs4 import BeautifulSoup
+import re
 
 
-class serverUpdate:
-    def __init__(self):
-        self.MCbedrock = "https://www.minecraft.net/en-us/download/server/bedrock"
-        self.MCjava = ""
-        headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br",
-            "DNT": "1", # Do Not Track
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1"
-        }
-    def _getContent(self):
-        session_data = requests.Session()
+_url='https://net-secondary.web.minecraft-services.net/api/v1.0/download/links'
+_headers={
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
 
+_Path = os.path.join(os.getcwd(), "tempFile")
+
+if not os.path.exists(_Path): 
+    os.mkdir(_Path) 
+
+def _getVersion(attempt_val=5) -> dict:
+    """get version and download link minecraft server.
+
+    Args:
+        attempt_val (int, optional): value for attempting. Defaults to 5.
+
+    Returns:
+        dict: result of version and link download minecraft server.
+    """
+    for attempt in range(attempt_val):
         try:
-            response = session_data.get(self.MCbedrock, headers=self.headers, timeout=20)
+            session = requests.get(
+                _url,
+                headers=_headers,
+                timeout=10
+            )
 
-            
-  
-        except requests.exceptions.RequestException as e:
-            print(f"Masih error: {e}")
+            return session.json()
+        
+        except requests.exceptions.RequestException as req_err:
+            wait_time = (attempt + 1) ** 2
+            print(f"Request Error: {req_err}. Retrying in {wait_time:.2f} seconds...")
+            time.sleep(wait_time)
+    raise Exception("Timeout, try again later")
+        
+def downloadVersion(url):
+    with requests.get(url=url, stream=True) as req:
+        req.raise_for_status()
+        name_file = re.split("/", url)[-2:]
+        full_path = os.path.join(_Path, "_".join(name_file))
+
+        with open(full_path, 'wb') as file:
+            for chunk in req.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print("Done downloading")    
     
-    def check(self):
-        pass
-  
-    def update(self):
-        pass
-  
-    def rollback(self):
-        pass
+content = _getVersion(3)
+print("Avaible version:")
+for num, link in enumerate(content["result"]["links"]):
+    versionType, downloadURL = link.values()
+    minecraVersion = re.split("/", downloadURL)[-2:]
+    print(f"{num + 1}. {versionType}: {"_".join(minecraVersion)}")
 
-
-
-    
-
-
+choosedVersion = int(input("Choose version that you want to download based order: ")) - 1
+full_link = content["result"]['links'][choosedVersion]["downloadUrl"]
+downloadVersion(full_link)
