9561a8c55200da7ef1f42db5eec3edd14c35c161
hanifnurfai <106461708+hanifnurfai@users.noreply.github.com>
2026-04-10 17:15:22 +0700

    Seperate download, extractor, file_ops to core. And updater to workflow. Also adding requirements.txt

---
+++ api/minecraft_api.py
@@ -0,0 +1,37 @@
+import logging
+import time
+import requests
+
+
+URL='https://net-secondary.web.minecraft-services.net/api/v1.0/download/links'
+HEADER ={
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+def _getVersion(attempt_val=5) -> dict:
+    """get version and download link minecraft server.
+
+    Args:
+        attempt_val (int, optional): value for attempting. Defaults to 5.
+
+    Returns:
+        dict: result of version and link download minecraft server.
+    """
+    for attempt in range(attempt_val):
+        try:
+            session = requests.get(
+                URL,
+                headers=HEADER,
+                timeout=10
+            )
+            logging.info("Request completed", )
+            return session.json()
+        
+        except requests.exceptions.RequestException as req_err:
+            wait_time = (attempt + 1) ** 2
+            logging.warning(f"Request Error: {req_err}. Retrying in {wait_time:.2f} seconds...")
+            time.sleep(wait_time)
+    logging.error("Download failed after %d retries.", attempt_val)
+    return None
